87b639c237e89948bbb5ef011f372d25d950f0d0
ahmad <aalmadani111@gmail.com>
2023-07-18 22:48:58 +0200

    include urls products in urls

--- project/urls.py
+++ project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', include('products.urls',namespace='products') ),
     path('summernote/', include('django_summernote.urls')),
 ]
