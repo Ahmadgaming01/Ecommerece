98f7d5f8d2c22298a8217906ec025b3c0a60446a
ahmad <aalmadani111@gmail.com>
2023-07-18 22:38:24 +0200

    install summernote , and register modles in admin site

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Product , ProductImages , Brand ,Review
 
 # Register your models here.
+
+class ProductImagesAdmin(admin.TabularInline):
+    model = ProductImages
+
+class ProductAdmin(SummernoteModelAdmin):
+    list_display = ['name','price','brand','flag']
+    list_filter = ['price', 'tags' , 'brand' ,'flag']
+    search_fields = ['name', 'subtitle' , 'description' ]
+    inlines = [ProductImagesAdmin]
+    summernote_fields = '__all__'
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(ProductImages)
+admin.site.register(Brand)
+admin.site.register(Review)
