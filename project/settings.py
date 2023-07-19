ea793ad5ff684e16cda2b0343feade3ec6e02b97
ahmad <aalmadani111@gmail.com>
2023-07-19 16:01:18 +0200

    add media and static settings

--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products',
     'taggit',
+    'django_summernote',
+
 ]
 
 MIDDLEWARE = [
@@ -118,6 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+MEDIA_URL = '/meadia/'
+MEDIA_ROOT = BASE_DIR / "meadia" 
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
