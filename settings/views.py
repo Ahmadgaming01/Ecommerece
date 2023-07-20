d57fc1fce522b02bf3cad054fb7766c1089d73d2
ahmad <aalmadani111@gmail.com>
2023-07-20 19:56:48 +0200

    update function in settings app ,add new,featrue,sale, for home page

--- settings/views.py
+++ settings/views.py
@@ -3,16 +3,17 @@ from products.models import Product , Brand , Review
 # Create your views here.
 def home (requset):
     brands =Brand.objects.all()
-    sale_products = Product.objects.filter(flag = 'Sale')
-    feature_products = Product.objects.filter(flag = 'feature')
-    new_products = Product.objects.filter(flag = 'new')
-    reviwes = Review.objects.all()
+    sale_products = Product.objects.filter(flag = 'Sale')[:10]
+    feature_products = Product.objects.filter(flag = 'Feature')[:6]
+    new_products = Product.objects.filter(flag = 'New')[:6]
+    reviwes = Review.objects.all()[:6]
 
     return render( requset , 'settings/home.html', {
         'brands':brands,
         'feature_products':feature_products,
         'sale_products' : sale_products,
+        'new_products' : new_products,
         'reviewes':reviwes,
-        
+        'brands_sliced' :brands[:6]
 
     })
