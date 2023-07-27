86ea40b7a973f0b16a6c1be7e3ed9973e9923725
ahmad <aalmadani111@gmail.com>
2023-07-27 21:22:10 +0200

    add new query set func + avg , sum , min , max ,

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render ,redirect
 from django.views import generic
 from.models import Product , ProductImages ,Brand ,Review
-from django.db.models import Q
+from django.db.models import Q , F , Value , Func
+from django.db.models.aggregates import Count , Avg ,Sum, Min,Max
 
 
 def post_list_debug (request):
@@ -19,13 +20,32 @@ def post_list_debug (request):
     #data = Review.objects.filter(create_date_month=7)
     #data = Product.objects.filter(price__gt=50, flag='Sale')
     #data = Product.objects.filter(price__gt = 20).filter(flag='Sale')\
-    data = Product.objects.filter(
-        Q(price__gt=50),
-        Q(flag = 'Sale')
-        
-
-    )
-
+    #data = Product.objects.filter(
+    #    Q(price__gt=50)|
+    #    ~ Q(flag = 'Sale'))
+    # data = Product.objects.filter(sku=F('price'))
+    #data = Product.objects.filter(sku=F('brand__id'))
+    #data = Product.objects.all().order_by('-name')  #reverse
+    #data = Product.objects.all().order_by('name')
+    #data = Product.objects.order_by('name')
+    #data = Product.objects.filter(brand_id=1).order_by('name')
+    #data = Product.objects.order_by('flag','name')
+    #data = Product.objects.all().order_by('name').last()
+    #data = Product.objects.all()[:5]
+    #data = Product.objects.all()[5:10]
+    #data = Product.objects.values('name','flag','price','brand__name')  #values dictionery
+    #data = Product.objects.values_list('name','flag','price','brand__name') #values tuple
+    #data = Product.objects.values('name','flag','price','brand__name').distinct()    #no repli.
+    #data = Product.objects.defer('tags')   #alle colons auser dem tags
+    #data = Product.objects.select_related('brand').all()  #use onToOne, OneToMany
+    #data = Product.objects.prefetch_related('brand').all() #use OnToMany
+    #data = Product.objects.aaggregate(Count('id'))
+    #data = Product.objects.aaggregate(Sum('id'))
+    #data = Product.objects.aaggregate(Max('id'))
+    #data = Product.objects.aaggregate(mymin = Min('price') , mycount = Count('id'))
+    #data = Product.objects.annotate(is_new = Value(True))
+    #data = Product.objects.annotate(is_tax = F('price')*1.2)
+    data = Brand.objects.annotate(posts = Count('Product_Brand'))
 
     return render(request , 'products/debug.html',{'data':data})
 
