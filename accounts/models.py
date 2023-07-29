232952e5c00b8e73418c35881e928996f42c9b82
ahmad <aalmadani111@gmail.com>
2023-07-29 07:07:48 +0200

    crete models for user app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+
+class Profile (models.Model):
+    user = models.ForeignKey(User , related_name= 'User_Profile' , on_delete= models.CASCADE)
+    image = models.ImageField(upload_to='accounts')
+
+
+PHONE_CHOICES = [
+    ('Primary','Primary')
+    ('Secondary','Secondary')
+    
+]
+ 
+class Phones (models.Model):
+    user = models.ForeignKey(User , related_name= 'User_Phones' , on_delete= models.CASCADE)
+    type = models.CharField(max_length=10 , choices=PHONE_CHOICES)
+    phone = models.CharField(max_length=25)
+
+
+ADRESS_CHOICES = [
+    ('Home','Home')
+    ('Office','Office')
+    ('Bussines','Bussines')
+    ('Academy','Academy')
+    ('Other','Other')
+]
+    
+
+class Adress(models.Model):
+    user = models.ForeignKey(User , related_name= 'User_Adress' , on_delete= models.SET_NULL)
+    type = models.TextField(max_length=150 , choices= ADRESS_CHOICES)
+
---
+++ project/anlaysis.md
@@ -0,0 +1,36 @@
+Product:
+    -name
+    -description
+    -images
+    -tags
+    -reviws
+    -sku
+    -brand
+    -flag[new ,sale , feature ]
+
+
+
+
+
+
+Brand:
+    -name
+    -image
+    
+
+
+
+
+Orders:
+    -code
+    -status [Recleced , Processed , shipped , Delivered]
+    -order Time
+    -Delivery Time
+    
+
+User:
+    -image
+    -name
+    -email
+    -phone_numbers
+    -adress
