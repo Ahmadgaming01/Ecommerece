d3486c9a0f649d288e82b0fad8cc913ea94321d2
ahmad <aalmadani111@gmail.com>
2023-07-31 18:47:21 +0200

    design user models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+from .models import User , Adress , Phones ,Profile
 # Register your models here.
--- accounts/models.py
+++ accounts/models.py
@@ -7,30 +7,35 @@ from django.contrib.auth.models import User
 class Profile (models.Model):
     user = models.ForeignKey(User , related_name= 'User_Profile' , on_delete= models.CASCADE)
     image = models.ImageField(upload_to='accounts')
-
+    def __str__(self):
+        return str(self.name)
+    
 
 PHONE_CHOICES = [
-    ('Primary','Primary')
-    ('Secondary','Secondary')
+    ('Primary','Primary'),
+    ('Secondary','Secondary'),
     
 ]
- 
+
 class Phones (models.Model):
     user = models.ForeignKey(User , related_name= 'User_Phones' , on_delete= models.CASCADE)
     type = models.CharField(max_length=10 , choices=PHONE_CHOICES)
     phone = models.CharField(max_length=25)
-
+    def __str__(self):
+        return str(self.name)
 
 ADRESS_CHOICES = [
-    ('Home','Home')
-    ('Office','Office')
-    ('Bussines','Bussines')
-    ('Academy','Academy')
-    ('Other','Other')
+    ('Home','Home'),
+    ('Office','Office'),
+    ('Bussines','Bussines'),
+    ('Academy','Academy'),
+    ('Other','Other'),
 ]
     
 
 class Adress(models.Model):
-    user = models.ForeignKey(User , related_name= 'User_Adress' , on_delete= models.SET_NULL)
+    user = models.ForeignKey(User , related_name= 'User_Adress' , on_delete= models.CASCADE)
     type = models.TextField(max_length=150 , choices= ADRESS_CHOICES)
 
+    def __str__(self):
+        return str(self.name)
