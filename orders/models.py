3951a1ea3162e262439c4c7437c4237e5fde6c20
ahmad <aalmadani111@gmail.com>
2023-08-17 20:07:12 +0200

    add cart details in models

--- orders/models.py
+++ orders/models.py
@@ -26,7 +26,7 @@ class Order(models.Model):
         return self.code
 
 class OrderDetail(models.Model):
-    oder = models.ForeignKey(Order , related_name='order_detail',on_delete= models.CASCADE)
+    order = models.ForeignKey(Order , related_name='order_detail',on_delete= models.CASCADE)
     product = models.ForeignKey(Product , related_name='order_product',on_delete= models.SET_NULL , null=True)
     price = models.FloatField()
     quantity = models.IntegerField()
@@ -34,7 +34,13 @@ class OrderDetail(models.Model):
         return self.str(self.order)
     
 class Cart(models.Model):
-    oder = models.ForeignKey(Order , related_name='order_detail',on_delete= models.CASCADE)
+    
     user = models.ForeignKey(User,related_name='cart_user', on_delete=models.SET_NULL , null=True ,blank=True)
+    completed = models.BooleanField(default=False)
+
+class CartDetail(models.Model):
+    cart = models.ForeignKey(Cart , related_name='cart_detail',on_delete=models.CASCADE)
+    oder = models.ForeignKey(Order , related_name='order_detail',on_delete= models.CASCADE)
     price = models.FloatField()
     quantity = models.IntegerField()
+    total = models.FloatField()
