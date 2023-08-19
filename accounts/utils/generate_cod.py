cae34e1b10c29c935a62173c19e20ed42230512c
ahmad <aalmadani111@gmail.com>
2023-08-19 23:33:20 +0200

    update models and related name

--- accounts/utils/generate_cod.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-
-def generate_code(length = 8):
-    data = "0123456789ABCDEFGHIJK"
-    code = ''.join(random.choice(data) for _ in range(length))
-    return code 
--- orders/models.py
+++ orders/models.py
@@ -3,20 +3,21 @@ from django.utils import timezone
 from accounts.models import Adress
 from products.models import Product
 from django.contrib.auth.models import User
+from utils.generate_cod import generate_code
 # Create your models here.
 
 
 
 ORDER_STATUS = (
-    ("Recieved" ,"Recieved")
-    ("Processed" ,"Processed")
-    ("Shipped" ,"Shipped")
+    ("Recieved" ,"Recieved"),
+    ("Processed" ,"Processed"),
+    ("Shipped" ,"Shipped"),
     ("Delivered" ,"Delivered")
 )
 
 class Order(models.Model):
     user = models.ForeignKey(User,related_name='order_user', on_delete=models.SET_NULL , null=True ,blank=True)
-    code = models.CharField(max_length=30)
+    code = models.CharField(max_length=30 , default=generate_code)
     status = models.CharField(max_length= 20 , choices=ORDER_STATUS)
     order_time = models.DateTimeField(default= timezone.now)
     delivery_time = models.DateTimeField(null=True , blank=True)
@@ -31,8 +32,8 @@ class OrderDetail(models.Model):
     price = models.FloatField()
     quantity = models.IntegerField()
     def __str__(self):
-        return self.str(self.order)
-    
+        return str(self.order)
+
 class Cart(models.Model):
     
     user = models.ForeignKey(User,related_name='cart_user', on_delete=models.SET_NULL , null=True ,blank=True)
@@ -40,7 +41,18 @@ class Cart(models.Model):
 
 class CartDetail(models.Model):
     cart = models.ForeignKey(Cart , related_name='cart_detail',on_delete=models.CASCADE)
-    oder = models.ForeignKey(Order , related_name='order_detail',on_delete= models.CASCADE)
+    oder = models.ForeignKey(Order , related_name='cart_product',on_delete= models.CASCADE)
     price = models.FloatField()
     quantity = models.IntegerField()
     total = models.FloatField()
+
+class Copon(models.Model):
+    code = models.CharField(max_length=25)
+    percentage = models.FloatField()
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(default=timezone.now)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.code
+    
