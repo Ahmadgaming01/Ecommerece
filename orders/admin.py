3a5e7219e6e62759fe4bd11852378ff2820667f4
ahmad <aalmadani111@gmail.com>
2023-08-19 23:38:52 +0200

    design for order app

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Order , OrderDetail , Cart , CartDetail , Copon
 
-# Register your models here.
+admin.site.register(Order)
+admin.site.register(OrderDetail)
+admin.site.register(Cart)
+admin.site.register(CartDetail)
+admin.site.register(Copon)
--- settings/admin.py
+++ settings/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Company
+from .models import Company , DeliveryFee
 # Register your models here.
 admin.site.register(Company)
+admin.site.register(DeliveryFee)
