2a983df56cb925ce9a434a06268a00a43a7f92e6
ahmad <aalmadani111@gmail.com>
2023-08-26 00:24:49 +0200

    add login and signup to base html

--- orders/cart_context_processor.py
+++ orders/cart_context_processor.py
@@ -2,6 +2,8 @@ from .models import Cart
 
 def get_cart_data (request):
     if request.user.is_authenticated:
-        
-    cart = Cart.objects.get(user = request.user)
-    return {'cart_data':cart}
+
+        cart = Cart.objects.get_or_create (user = request.user , completed = False)
+        return {'cart_data':cart}
+    else:
+        return{}
