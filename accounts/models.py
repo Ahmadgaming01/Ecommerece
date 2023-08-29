00c7679c6244012381f3dfec53537b37d0fd9ab1
ahmad <aalmadani111@gmail.com>
2023-08-29 23:54:00 +0200

    add info or login signup in navbar

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 
 class Profile (models.Model):
-    user = models.ForeignKey(User , related_name= 'User_Profile' , on_delete= models.CASCADE)
+    user = models.ForeignKey(User , related_name= 'user_profile' , on_delete= models.CASCADE)
     image = models.ImageField(upload_to='accounts')
     code = models.CharField(max_length=10)
     def __str__(self):
