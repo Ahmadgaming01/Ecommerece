0a29334a9b70174d80045653a45fc1beead63f73
ahmad <aalmadani111@gmail.com>
2023-09-01 22:18:56 +0200

    add add_to_cart_view in templete

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.views.generic import ListView
-from.models import Order
+from.models import Order , Cart , CartDetail
+from products.models import Product
 # Create your views here.
 
 class OderList(ListView):
     model = Order
+
+def add_to_cart (request):
+    product = Product.objects.get(id=request.POST['product_id'])
+    quantity = request.POST['quantity']
+
+
+    cart = Cart.objects.get(user = request.user , completed = False)
+    cart_detail , created = CartDetail.objects.get_or_create (cart=cart , product=product)
+    cart_detail.quantity = quantity
+    cart_detail.price = product.price
+    cart_detail.total = int(quantity) * product.price
+    cart_detail.save()
+
+    return redirect(f'/product/{{product.slug}}')
