012cc72085f4b2684745242467588c501d5ac5ab
ahmad <aalmadani111@gmail.com>
2023-09-09 18:23:00 +0200

    show all related in serializer

--- products/seriallizers.py
+++ products/seriallizers.py
@@ -2,15 +2,17 @@ from rest_framework import serializers
 from .models import Product , Brand
 
 
-class ProductSerializer (serializers.ModelSerializer):
-    
-    class  Meta:
-      model = Product
-      fields = '__all__'
+
 
 
 class BrandSerializer (serializers.ModelSerializer):
-    brand = serializers.StringRelatedField()
+    
     class  Meta:
       model = Brand
+      fields = '__all__'
+
+class ProductSerializer (serializers.ModelSerializer):
+    brand = BrandSerializer()
+    class  Meta:
+      model = Product
       fields = '__all__'
