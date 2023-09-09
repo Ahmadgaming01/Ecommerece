7563938161eccfa3d178e3b0c009f599f227c633
ahmad <aalmadani111@gmail.com>
2023-09-09 18:00:21 +0200

    add Products list and details api use classbasedview

--- products/api.py
+++ products/api.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import generics
 from .seriallizers import ProductSerializer , BrandSerializer
 from .models import Product , Brand
-
+'''
 @api_view(['GET'])
 def product_list_api (request):
     products = Product.objects.all()
@@ -15,6 +15,16 @@ def product_detail_api (request , product_id):
     products = Product.objects.get(id=product_id )
     data = ProductSerializer(products , context={'request':request}).data
     return Response ({'data':data})
+'''
+
+class ProductListApi(generics.ListAPIView):
+    products = Product.objects.all() 
+    serializer_class = ProductSerializer
+
+class ProductDetailApi(generics.RetrieveAPIView):
+    products = Product.objects.all() 
+    serializer_class = BrandSerializer
+
 
 
 class BrandListApi (generics.ListAPIView):
--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import ProductList , ProductDetail , BrandList,BrandDetail,post_list_debug
-from .api import product_list_api , product_detail_api , BrandListApi
+from .api import ProductListApi , ProductDetailApi , BrandListApi
 app_name = 'products'
 urlpatterns = [
     path('debug', post_list_debug),
@@ -8,7 +8,7 @@ urlpatterns = [
     path('<slug:slug>',ProductDetail.as_view(),name='product_detail'),
     path('Brand/' , BrandList.as_view(),name='brand_list'),
     path('Brand/<slug:slug>',BrandDetail.as_view(),name='brand_detail'),\
-    path('api/list' , product_list_api),    
-    path('api/list/<int:product_id>' , product_detail_api),
+    path('api/list' , ProductListApi.as_view()),    
+    path('api/list/<int:product_id>' , ProductDetailApi.as_view()),
     path('brands/api/list' , BrandListApi.as_view()),
 ]
