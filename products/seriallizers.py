1a3dbed2fb4170171c77e5abaadfe72b5661e648
ahmad <aalmadani111@gmail.com>
2023-09-09 18:19:16 +0200

    add stringrelated in serializer api

--- products/seriallizers.py
+++ products/seriallizers.py
@@ -3,12 +3,14 @@ from .models import Product , Brand
 
 
 class ProductSerializer (serializers.ModelSerializer):
+    
     class  Meta:
       model = Product
       fields = '__all__'
 
 
 class BrandSerializer (serializers.ModelSerializer):
+    brand = serializers.StringRelatedField()
     class  Meta:
       model = Brand
       fields = '__all__'
