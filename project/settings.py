15c9969c66c18af9f14f10b90ab9cf018819791b
ahmad <aalmadani111@gmail.com>
2023-09-18 21:56:48 +0200

    add jwt library to sittings and url

--- project/settings.py
+++ project/settings.py
@@ -44,16 +44,19 @@ INSTALLED_APPS = [
     'settings',
     "debug_toolbar",
     "orders",
+
+    #-----api------
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
+    'rest_framework_simplejwt',
     
 
 ]
 
 REST_FRAMEWORK = {
         'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,12 @@ from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
 from settings.views import home
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path('accounts',include('accounts.urls')),
@@ -28,6 +34,8 @@ urlpatterns = [
     path('orders/', include('orders.urls',namespace='orders') ),
     path('summernote/', include('django_summernote.urls')),
     path('',home),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
 ]
 
