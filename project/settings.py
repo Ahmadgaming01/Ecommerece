aaef2b3af082d466c1cd7da7bca36ef7137af3a2
ahmad <aalmadani111@gmail.com>
2023-10-03 17:06:37 +0200

    aad email settings in settings

--- project/settings.py
+++ project/settings.py
@@ -176,7 +176,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 
 # ------------- end time token--------------#
@@ -204,3 +204,11 @@ LANGUAGES = [
 
 
 AUTHENTICATION_BACKENDS=['accounts.backends.EmailBackend']
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "dev.test.mail66@gmail.com"
+EMAIL_HOST_PASSWORD = ""
