109c60d6173ec3fc5365608085479cd036722bba
ahmad <aalmadani111@gmail.com>
2023-10-03 12:42:32 +0200

    add login template and custom class in form

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,13 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class SignupForm (UserCreationForm):
+    
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+        'class':'form-control',
+        'placeholder':'Username'
+        }
+    ))
     class Meta:
         model = User
         fields=['username','email', 'password1', 'password2']
