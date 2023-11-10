19a04aca030abaf6eee17eed8de87035e24b15a9
ahmad <aalmadani111@gmail.com>
2023-11-11 00:06:53 +0100

    fixed error in settings for redis server

--- project/settings.py
+++ project/settings.py
@@ -216,12 +216,14 @@ EMAIL_HOST_PASSWORD = ""
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+        "LOCATION": "redis://redis:6379/0",
     }
 }
 
-CELERY_BROCKER_URL = "redis://localhost:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+#CELERY_BROCKER_URL = "redis://localhost:6379/0"
+#CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_BROCKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
