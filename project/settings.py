4ac6b6a90b203ef32679eb71f5cc31931acb1c3f
ahmad <aalmadani111@gmail.com>
2023-12-14 23:49:39 +0100

    create postgresdatabase

--- project/settings.py
+++ project/settings.py
@@ -118,25 +118,25 @@ INTERNAL_IPS = [
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# postgres db settings 
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "amazon",
-#         "USER": "postgres",
-#         "PASSWORD":"postgres",
-#         "HOST":"db",
-#         "PORT":"5432",
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+#postgres db settings 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "amazon",
+        "USER": "postgres",
+        "PASSWORD":"postgres",
+        "HOST":"db",
+        "PORT":"5432",
+    }
+}
 
 
 # Password validation
